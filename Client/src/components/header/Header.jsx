c7b74e695a0544d28fb2fa0471c816cf7594fed6
nischay <nischayjain4948@gmail.com>
2022-02-14 09:10:35 +0530

    ic

--- Client/src/components/header/Header.jsx
+++ Client/src/components/header/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
     <div className="header">
       <div className="headerTitles">
         <span className="headerTitleSm">SHUTTER/UPP</span>
-        <span className="headerTitleLg">BLOG</span>
+        <span className="headerTitleLg">wriye your story....</span>
       </div>
       <img
         className="headerImg"
-        src="https://www.instagram.com/p/CZY9GZoPpi4/?utm_medium=copy_link"
+        src="https://lh3.googleusercontent.com/pw/AM-JKLU1VUrn3aikQUTzCAwQdC_YXHfJZF1ECmohZl-fQn5ZbQ_zW6PGNzpPtyrCRca6drSSCKW7YsFSY1G2nhou4FyW6A_oHcMXsI42rQvS_bqr69_CcgZwmQJgkiyPL49hc7XE8Wq47aQXiEC1sokFtQVK=s500-no?authuser=0"
         alt=""
       />
     </div>
